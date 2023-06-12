d64a4454869d1cfa5341eab65d193ba6f3bdbd9f
Grant Zukowski <gzukowski@aampglobal.com>
2023-06-12 00:06:34 -0500

    fix: :bug: make the images work and use a data model
    Pull images from a url in an images table.  Could be extended to use an s3 bucket.

---
+++ lib/dog_breeds/images/image.ex
@@ -0,0 +1,17 @@
+defmodule DogBreeds.Images.Image do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "image" do
+    belongs_to :dog, DogBreeds.Breeds.Dog
+    field :url, :string
+
+    timestamps()
+  end
+
+  def changeset(image, attrs) do
+    image
+    |> cast(attrs, [:url])
+    |> validate_required([:url])
+  end
+end
